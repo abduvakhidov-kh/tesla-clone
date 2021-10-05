e24351c2c36f858d0728c89454ec55ed549dd233
Abduvakhidov Khabibullo <habibulloabduvahidov03@gmail.com>
2021-10-05 22:28:45 +0500

    additional sections

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,7 +5,41 @@ import Section from "../section/Section";
 export default function Home() {
   return (
     <Container>
-      <Section />
+      <Section 
+        title="Model S"
+        desc="Order Online for Touchless Delivery"
+        lfbtn="Custom Order"
+        rgbtn="Existing Inventory"
+        bgImage="model-s.jpg"
+      />
+      <Section 
+        title="Model 3"
+        desc="Order Online for Touchless Delivery"
+        lfbtn="Custom Order"
+        rgbtn="Existing Inventory"
+        bgImage="model-3.jpg"
+      />
+      <Section 
+        title="Model X"
+        desc="Order Online for Touchless Delivery"
+        lfbtn="Custom Order"
+        rgbtn="Existing Inventory"
+        bgImage="model-x.jpg"
+      />
+      <Section 
+        title="Model Y"
+        desc="Order Online for Touchless Delivery"
+        lfbtn="Custom Order"
+        rgbtn="Existing Inventory"
+        bgImage="model-y.jpg"
+      />
+      <Section 
+        title="Model Y"
+        desc="Order Online for Touchless Delivery"
+        lfbtn="Custom Order"
+        rgbtn="Existing Inventory"
+        bgImage="model-y.jpg"
+      />
     </Container>
   );
 }
