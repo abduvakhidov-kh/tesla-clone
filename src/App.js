8528b90876b3eb877d802a0d0b9320686c52e8f9
Abduvakhidov Khabibullo <habibulloabduvahidov03@gmail.com>
2021-10-05 21:31:24 +0500

    section component has done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Header from './components/header/Header.js';
-import Home from './components/home/Home';
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Header from "./components/header/Header.jsx";
+import Home from "./components/home/Home";
 
 function App() {
   return (
--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -8,10 +8,13 @@ export default function Section() {
         <h1>Model S</h1>
         <p>Order Online for Touchless Delivery</p>
       </ItemText>
-      <ButtonGroup>
-        <LeftButton>Custom Order</LeftButton>
-        <RightButton>Existing Inventory</RightButton>
-      </ButtonGroup>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom Order</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
   );
 }
@@ -21,11 +24,19 @@ const Wrap = styled.div`
   height: 100vh;
   background-image: url("/images/model-3.jpg");
   background-repeat: no-repeat;
-  background-size: center;
+  background-size: cover;
   background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
-const ButtonGroup = styled.div``;
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+`;
 const LeftButton = styled.div`
+  cursor: pointer;
   background-color: rgba(23, 26, 32, 0.8);
   height: 40px;
   width: 256px;
@@ -33,6 +44,11 @@ const LeftButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  margin: 8px;
 `;
 
 const RightButton = styled(LeftButton)``;
@@ -40,4 +56,16 @@ const RightButton = styled(LeftButton)``;
 const ItemText = styled.div`
   padding-top: 15vh;
   text-align: center;
+`;
+
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
